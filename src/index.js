9eaa9c0ec670188dbe0fb58a8b8415f446bf471c
contactteamprofessionaldesigns-gif <contactteam.professionaldesigns@gmail.com>
2026-09-22 16:48:38 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@aws-amplify/ui-react/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+
+// Configure Amplify
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.REACT_APP_USER_POOL_ID,
+      userPoolClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+      identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+    }
+  },
+  API: {
+    GraphQL: {
+      endpoint: process.env.REACT_APP_API_ENDPOINT || 'https://your-api-endpoint.amazonaws.com/graphql',
+      region: process.env.REACT_APP_AWS_REGION || 'ap-south-1',
+      defaultAuthMode: 'userPool'
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
